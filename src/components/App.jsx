38fa0e68f2a7326541b2b05a485e00973da72c40
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-10-14 15:45:16 +0300

    add class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,10 @@
-
 import { GlobalStyle } from './GlobalStyle';
-
+import { Counter } from './counter/counter';
 
 export const App = () => {
   return (
-    <><div className="FeedBack">
-    <h1>Please leave feedback</h1>
-
-    <button type="button" onClick={(e)=>{console.log(e)}}>Good</button>
-      <button type="button" onClick={(e) => { console.log(e) }}>Neutral</button>
-      <button type="button" onClick={(e) => { console.log(e) }}>Bad</button>
-
-    <h1>Statistics</h1>
-    <p>Good</p>
-    <p>Neutral</p>
-    <p>Bad</p>
-    <p>Total:</p>
-    <p>Positive feedback:</p>
-  </div>
+    <>
+      <Counter />
       <GlobalStyle />
     </>
   );
---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+
+export class Counter extends Component {
+  render() {
+    return (
+      <div className="FeedBack">
+        <h1>Please leave feedback</h1>
+
+        <button type="button" onClick={(e)=>{console.log(e)}}>Good</button>
+        <button type="button" onClick={(e)=>{console.log(e)}}>Neutral</button>
+        <button type="button" onClick={(e)=>{console.log(e)}}>Bad</button>
+
+        <h1>Statistics</h1>
+        <p>Good</p>
+        <p>Neutral</p>
+        <p>Bad</p>
+        <p>Total:</p>
+        <p>Positive feedback:</p>
+      </div>
+    );
+  }
+}
