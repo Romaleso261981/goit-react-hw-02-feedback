181db855eac653d85a91c990b89f9fddb4f509cf
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2022-10-15 09:14:25 +0300

    Update counter.jsx

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,14 +1,48 @@
 import { Component } from 'react';
 
 export class Counter extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    
+  }
+
+  countPositiveFeedbackPercentage() {
+    
+  }
   render() {
     return (
       <div className="FeedBack">
         <h1>Please leave feedback</h1>
 
-        <button type="button" onClick={(e)=>{console.log(e)}}>Good</button>
-        <button type="button" onClick={(e)=>{console.log(e)}}>Neutral</button>
-        <button type="button" onClick={(e)=>{console.log(e)}}>Bad</button>
+        <button
+          type="button"
+          onClick={e => {
+            console.log(e);
+          }}
+        >
+          Good
+        </button>
+        <button
+          type="button"
+          onClick={e => {
+            console.log(e);
+          }}
+        >
+          Neutral
+        </button>
+        <button
+          type="button"
+          onClick={e => {
+            console.log(e);
+          }}
+        >
+          Bad
+        </button>
 
         <h1>Statistics</h1>
         <p>Good</p>
